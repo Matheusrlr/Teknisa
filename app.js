2f8cfbdf1346046c70db7019ec2a0bf6d4923330
Oblivion7 <matheus.reis13@hotmail.com>
2019-09-10 14:45:37 -0300

    quase lá

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = new Vue({
         newTodo: '',
         desc: '',
         number: 1,
+        capacidade_maxima : 13,
         todos: [],
         descricao: [],
         valor: []
@@ -42,9 +43,42 @@ const app = new Vue({
         this.todos.forEach(todo => {
             todo.done = true;
           });
-        }
+        },
 
+        compare( a, b ) {
+            if ( a.title2 < b.title2){
+              return 1;
+            }
+            if ( a.title2 > b.title2){
+              return -1;
+            }
+            return 0;
+          },
+          
+          knapSack(valor, capacidade_maxima){
+            capacidade_maxima = 13
+            valor = valor[i]
+            dia = 1
 
-    }
+            while (valor != 0){
+
+                capacidade_dia = 0
+                index = 0
+                console.log("Dia: ", dia)
+                while((valor) > 0  && capacidade_dia + valor[index] <= 13){
+                    
+                    capacidade_dia += valor[index]
+                    console.log(" Peso: ", valor[index])
+                    valor.pop(index)
+
+                    dia += 1
+                }
+                }
+
+          }
+          
+        
+    }  
+    
 
 });
